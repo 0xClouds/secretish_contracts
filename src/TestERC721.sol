b3dc381e72119043cff5c150b6f75413c106892c
Cloud <cloud.frontier0@gmail.com>
2024-02-15 21:26:22 -0800

    add: test for new giveERC721 function

--- src/TestERC721.sol
+++ src/TestERC721.sol
@@ -4,13 +4,12 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TestERC721 is ERC721 {
+    uint256 tokenId = 1;
+
     constructor() ERC721("TestERC", "TEST") {}
 
-    function mint(address to, uint256 tokenId) public {
+    function mint(address to) public {
         _safeMint(to, tokenId);
-    }
-
-    function transfer(address to, uint256 tokenId) public {
-        _transfer(msg.sender, to, tokenId);
+        tokenId += 1;
     }
 }
